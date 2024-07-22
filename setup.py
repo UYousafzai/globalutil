c96fbbb5e5cb37701c2a91c3c1ad854aef3d0a94
UmarYousafzai <umar.khan@deepreader.de>
2024-07-22 18:37:35 +0200

    fix: built using setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="globalutil",
-    version="0.3.7",
+    version="0.3.8",
     author="Umar Khan",
     author_email="umaryousafzai9@gmail.com",
     description="A global utility package for all the data sorting and inspection needs.",
